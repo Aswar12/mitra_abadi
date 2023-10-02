1cd50d67cb0addfa28fc5f82f76b7b8be6e63efa
FIRMANGANTENG46 <firmansyah18@mhs.akba.ac.id>
2023-10-02 13:29:29 +0800

    sdds

--- resources/views/eoq/index.blade.php
+++ resources/views/eoq/index.blade.php
@@ -13,4 +13,129 @@
             </div>
         </div>
     </div>
+
+
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">EOQ Calculation</div>
+
+                    <div class="card-body">
+                        <h1>Hasil Perhitungan EOQ</h1>
+                        <p>EOQ untuk barang tertentu adalah: {{ $eoq }}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">EOQ Calculation</div>
+
+                    <div class="card-body">
+                        <h1>Hasil Perhitungan EOQ</h1>
+                        <p>EOQ untuk barang tertentu adalah: {{ $eoq }}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">EOQ Calculation</div>
+
+                    <div class="card-body">
+                        <h1>Hasil Perhitungan EOQ</h1>
+                        <p>EOQ untuk barang tertentu adalah: {{ $eoq }}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">EOQ Calculation</div>
+
+                    <div class="card-body">
+                        <h1>Hasil Perhitungan EOQ</h1>
+                        <p>EOQ untuk barang tertentu adalah: {{ $eoq }}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">EOQ Calculation</div>
+
+                    <div class="card-body">
+                        <h1>Hasil Perhitungan EOQ</h1>
+                        <p>EOQ untuk barang tertentu adalah: {{ $eoq }}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">EOQ Calculation</div>
+
+                    <div class="card-body">
+                        <h1>Hasil Perhitungan EOQ</h1>
+                        <p>EOQ untuk barang tertentu adalah: {{ $eoq }}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">EOQ Calculation</div>
+
+                    <div class="card-body">
+                        <h1>Hasil Perhitungan EOQ</h1>
+                        <p>EOQ untuk barang tertentu adalah: {{ $eoq }}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">EOQ Calculation</div>
+
+                    <div class="card-body">
+                        <h1>Hasil Perhitungan EOQ</h1>
+                        <p>EOQ untuk barang tertentu adalah: {{ $eoq }}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
 </x-app-layout>
