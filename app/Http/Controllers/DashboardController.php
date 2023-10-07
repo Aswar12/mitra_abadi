90f65254d0dd5de41931b9799723e833a88cf568
FIRMANGANTENG46 <firmansyah18@mhs.akba.ac.id>
2023-10-07 11:54:38 +0800

    cdone kak

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -7,12 +7,35 @@ use App\Models\Sale;
 
 class DashboardController extends Controller
 {
-    public function index()
-    {
-        $items = Item::all();
-        $procurements = Procurement::all();
-        $sales = Sale::all();
+    // public function index()
+    // {
+    //     $items = Item::all();
+    //     $procurements = Procurement::all();
+    //     $sales = Sale::all();
 
-        return view('dashboard.index', compact('items', 'procurements', 'sales'));
-    }
+    //     return view('dashboard.index', compact('items', 'procurements', 'sales'));
+    // }
+    // public function index()
+    // {
+    //     $items = Item::all();
+
+    //     $itemNames = $items->pluck('name');
+    //     $itemPrices = $items->pluck('unit_price');
+
+    //     return view('dashboard.index', compact('itemNames', 'itemPrices'));
+    // }
+    public function index()
+        {
+            $items = Item::all();
+            $procurements = Procurement::all();
+            $sales = Sale::all();
+
+            $itemNames = $items->pluck('name');
+            $itemPrices = $items->pluck('unit_price');
+
+            
+            $procurementValues = $procurements->pluck('order_quantity'); // Ganti 'value' dengan nama kolom yang sesuai
+
+            return view('dashboard.index', compact('itemNames', 'itemPrices', 'procurementValues'));
+        }
 }
--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -3,40 +3,49 @@
         <div class="overflow-hidden bg-white rounded-lg shadow-xl dark:bg-white sm:rounded-lg ">
             <h1 class="py-2 mb-2 text-lg font-bold text-center">Dashboard</h1>
 
-                <div class="container">
-                    <div class="row justify-content-center">
-                        <div class="col-md-12">
-                            <div class="card">
-                                <div class="card-body">
-                                    <h1>Selamat Datang di Dashboard</h1>
-                                    <p>Di sini Anda dapat melihat informasi dari seluruh tabel.</p>
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-md-12">
+                        <div class="card">
+                            <div class="card-body">
+                                <canvas id="combinedChart" width="400" height="200"></canvas>
 
-                                    <h2>Data Barang</h2>
-                                    <table class="min-w-full border rounded-lg dark:border-gray-200">
-                                        <thead>
-                                            <tr  class="bg-gray-100 rounded-lg dark:bg-gray-100 ">
-                                                <th class="px-1 py-1 border dark:border-black">Nama</th>
-                                                <th class="px-1 py-1 border dark:border-black">Deskripsi</th>
-                                                <th class="px-1 py-1 border dark:border-black">Harga Satuan</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            @foreach ($items as $item)
-                                                <tr class="bg-white dark:bg-gray-100">
-                                                    <td class="px-6 py-3 border dark:border-black">{{ $item->name }}</td>
-                                                    <td class="px-6 py-3 border dark:border-black">{{ $item->description }}</td>
-                                                    <td class="px-6 py-3 border dark:border-black">{{ $item->unit_price }}</td>
-                                                </tr>
-                                            @endforeach
-                                        </tbody>
-                                    </table>
-
-                                    <!-- Tambahkan tabel dan data lainnya sesuai dengan kebutuhan -->
-                                </div>
+                                <script>
+                                    document.addEventListener('DOMContentLoaded', function () {
+                                        var ctx = document.getElementById('combinedChart').getContext('2d');
+                                        var myChart = new Chart(ctx, {
+                                            type: 'bar',
+                                            data: {
+                                                labels: {!! json_encode($itemNames) !!},
+                                                datasets: [{
+                                                    label: 'Harga Satuan',
+                                                    data: {!! json_encode($itemPrices) !!},
+                                                    backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                                                    borderColor: 'rgba(75, 192, 192, 1)',
+                                                    borderWidth: 1
+                                                }, {
+                                                    label: 'Nilai Pengadaan',
+                                                    data: {!! json_encode($procurementValues) !!},
+                                                    backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                                                    borderColor: 'rgba(255, 99, 132, 1)',
+                                                    borderWidth: 1
+                                                }]
+                                            },
+                                            options: {
+                                                scales: {
+                                                    y: {
+                                                        beginAtZero: true
+                                                    }
+                                                }
+                                            }
+                                        });
+                                    });
+                                </script>
                             </div>
                         </div>
                     </div>
                 </div>
+            </div>
         </div>
     </div>
 </x-app-layout>
