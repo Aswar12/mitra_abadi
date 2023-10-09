bbc67a49a68998e79b115766ead1d2363cf19a89
fialsyahran1 <142391252+fialsyahran1@users.noreply.github.com>
2023-10-09 14:32:38 +0800

    sidebar

--- resources/views/livewire/sidebar.blade.php
+++ resources/views/livewire/sidebar.blade.php
@@ -15,9 +15,7 @@
                 <span>Dashboard</span>
             </a>
         </li>
-        @endif
-
-         <li>
+        <li>
             <a href="{{ route('eoq.index') }}"
                 class="flex items-center p-2 space-x-2 text-white rounded-lg hover:bg-gray-700">
                 <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 640 512">
@@ -38,6 +36,8 @@
                 <span>Perhitungan Metode</span>
             </a>
         </li>
+        @endif
+
 
         <li>
             <a href="{{ route('items.index') }}"
